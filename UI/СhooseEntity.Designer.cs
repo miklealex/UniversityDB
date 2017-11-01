8187d39992d5925fccc11a23c42f0e49951c419b
moleksyn <miklealex73@gmail.com>
2017-11-01 18:12:47 +0200

    added images to listView on chooseEntity form, removed combobox from there

--- UI/СhooseEntity.Designer.cs
+++ UI/СhooseEntity.Designer.cs
@@ -30,8 +30,6 @@
         {
             this.btnOk = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
-            this.comboBox = new System.Windows.Forms.ComboBox();
-            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // btnOk
@@ -59,34 +57,11 @@
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
-            // comboBox
-            // 
-            this.comboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox.FormattingEnabled = true;
-            this.comboBox.Location = new System.Drawing.Point(12, 12);
-            this.comboBox.Name = "comboBox";
-            this.comboBox.Size = new System.Drawing.Size(253, 21);
-            this.comboBox.TabIndex = 2;
-            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
-            // 
-            // listView1
-            // 
-            this.listView1.Location = new System.Drawing.Point(13, 58);
-            this.listView1.Name = "listView1";
-            this.listView1.ShowGroups = false;
-            this.listView1.Size = new System.Drawing.Size(252, 323);
-            this.listView1.TabIndex = 5;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
             // СhooseEntity
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(471, 427);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.comboBox);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOk);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -100,7 +75,5 @@
 
         private System.Windows.Forms.Button btnOk;
         private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.ComboBox comboBox;
-        private System.Windows.Forms.ListView listView1;
     }
 }
--- UI/СhooseEntity.cs
+++ UI/СhooseEntity.cs
@@ -13,19 +13,11 @@ namespace UI
     public partial class СhooseEntity : Form
     {
         public string SelectedType;
-
+        private ListView listView1;
         public СhooseEntity(List<string> types)
         {
+            CreateMyListView(types);
             InitializeComponent();
-
-            comboBox.Items.AddRange(types.ToArray());
-
-            foreach (string obj in types)
-            {
-                ListViewItem lvi = new ListViewItem();
-                lvi.Text = obj;
-                listView1.Items.Add(lvi);
-            }
             
         }
 
@@ -39,10 +31,64 @@ namespace UI
             Close();
         }
 
-        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        private void listView_SelIndexChanged(object sender, EventArgs e)
         {
-            SelectedType = comboBox.SelectedItem.ToString();
-            btnOk.Enabled = true;
+            if (listView1.SelectedItems.Count == 1)
+            {
+                SelectedType = listView1.SelectedItems[0].Text;
+                btnOk.Enabled = true;
+            }
+            else
+            {
+                btnOk.Enabled = false;
+            }
+        }
+
+        private void listView_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left && Control.ModifierKeys != Keys.Shift)
+            {
+                
+            }
+
+        }
+
+        private void CreateMyListView(List<string> types)
+        {
+            // Create a new ListView control.
+            listView1 = new ListView();
+            listView1.Bounds = new Rectangle(new Point(10, 10), new Size(300, 400));
+
+            // Set the view to show details.
+            listView1.View = View.LargeIcon;
+            // Allow the user to edit item text.
+            listView1.LabelEdit = true;
+            // Allow the user to rearrange columns.
+            listView1.AllowColumnReorder = true;
+            // Select the item and subitems when selection is made.
+            listView1.FullRowSelect = true;
+            // Display grid lines.
+            listView1.GridLines = true;
+
+            listView1.Scrollable = true;
+
+            foreach (string obj in types)
+            {
+                listView1.Items.Add(obj,0);
+            }
+            // Create two ImageList objects.
+            ImageList imageListSmall = new ImageList();
+            ImageList imageListLarge = new ImageList();
+
+            imageListLarge.Images.Add(Bitmap.FromFile("D:\\VSprojects\\UniversityDB\\UniversityDB\\images\\File.bmp"));
+
+            //Assign the ImageList objects to the ListView.
+            listView1.LargeImageList = imageListLarge;
+
+            // Add the ListView to the control collection.
+            this.Controls.Add(listView1);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView_SelIndexChanged);
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView_MouseDown);
         }
     }
 }
